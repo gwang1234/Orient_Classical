9056294bc07a1643a855238e1f499bf4da2e9ca2
gwang1234 <qoi11@naver.com>
2024-12-28 23:57:32 +0900

    12월 28일일

--- practice/ex_view/exurls.py
+++ practice/ex_view/exurls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('get1/', views.get1),
+    path('get2/<int:n1>/<int:n2>/<str:n3>/', views.get2),
+    path('post1/', views.post1),
 ]
--- practice/ex_view/views.py
+++ practice/ex_view/views.py
@@ -7,3 +7,32 @@ def index(request):
     now = timezone.now()
     print("현재 시간: ",now)
     return render(request, "ex_view/index.html", {'now':now})
+
+def get1(request):
+    print(request.GET)
+    keys = request.GET.keys()
+    for key in keys:
+        value = request.GET[key]
+        print(f'{key}:{value}')
+    
+    num = int(request.GET['n1']) + int(request.GET['n2'])
+    print(num)
+    return HttpResponse('get')
+
+def get2(request, n1, n2, n3):
+    print("n1: ",n1)
+    print("n2: ",n2)
+    print("n3: ",n3)
+    print(n1 + n2)
+    return HttpResponse('get2')
+
+def post1(request):
+    print(request.GET)
+    keys = request.POST.keys()
+    for key in keys:
+        value = request.POST[key]
+        print(f'{key}:{value}')
+    
+    num = int(request.POST['n1']) + int(request.POST['n2'])
+    print(num)
+    return HttpResponse('post1')
