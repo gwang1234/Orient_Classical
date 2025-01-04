4d5643d911d5bf427c19391c1d125b72ab7c920d
gwang1234 <qoi11@naver.com>
2025-01-04 22:17:16 +0900

    클래스형 뷰

--- practice/ex_view/exurls.py
+++ practice/ex_view/exurls.py
@@ -3,10 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 함수형 뷰
     path('',views.index),
     path('get1/', views.get1),
     path('get2/<int:n1>/<int:n2>/<str:n3>/', views.get2),
     path('post1/', views.post1),
     path('post2/<str:msg>/<int:n>/', views.post2),
     path('getpost1/', views.getpost1),
+    
+    # 클래스형 뷰
+    path('get3/', views.ExamGet3.as_view()),
+    path('get4/<int:n1>/<int:n2>/<str:n3>/', views.ExamGet4.as_view()),
+    path('post3/', views.ExamPost3.as_view()),
+    path('post4/<str:msg>/<int:n>/', views.ExamPost4.as_view()),
+    path('getpost2/', views.ExamGetPost.as_view()),
 ]
--- practice/ex_view/views.py
+++ practice/ex_view/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -48,3 +48,64 @@ def post2(request, msg, n):
 def getpost1(request):
     print(request.method)
     return HttpResponse('getpost1')
+
+
+# 클래스형 뷰 
+from django.views.generic import View
+
+class ExamGet3(View):
+    def get(self, request):
+        print('get3/ 요청이 들어옴 ')
+        keys = request.GET.keys()
+        for key in keys:
+            value = request.GET[key]
+            print(f'{key}:{value}')
+        
+        num = int(request.GET['n1']) + int(request.GET['n2'])
+        print(num)
+        return HttpResponse('get3')
+    
+class ExamGet4(View):
+    def get(self, request, n1, n2, n3):
+        print("n1: ",n1)
+        print("n2: ",n2)
+        print("n3: ",n3)
+        print(n1 + n2)
+        return HttpResponse('get4')
+    
+class ExamPost3(View):
+    def post(self, request):
+        keys = request.POST.keys()
+        for key in keys:
+            value = request.POST[key]
+            print(f'{key}:{value}')
+        
+        num = int(request.POST['n1']) + int(request.POST['n2'])
+        print(num)
+        return HttpResponse('post3')
+    
+class ExamPost4(View):
+    def post(self, request, msg, n):
+        print("msg: ", msg)
+        print("n: ", n)
+        keys = request.POST.keys()
+        for key in keys:
+            value = request.POST[key]
+            print(f'{key}:{value}')
+        return HttpResponse('post4')
+    
+class ExamGetPost(View):
+    def get(self, request):
+        print("get요청 동작")
+        return HttpResponse('getpost2/(get)')
+    
+    def post(self, request):
+        print("Post요청 동작")
+        user = request.POST['user']
+        pwd = request.POST['pwd']
+        if user == pwd:
+            print('로그인 성공')
+            return HttpResponse('getpost2/(Post)')
+        else:
+            print('로그인 실패')
+            return HttpResponseRedirect('/ex_view/')
