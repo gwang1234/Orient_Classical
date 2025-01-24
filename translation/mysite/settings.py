76c206b6e089f80997e443695dae170298f8635c
gwang1234 <qoi11@naver.com>
2025-01-24 22:17:54 +0900

    vite react

--- translation/mysite/settings.py
+++ translation/mysite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # CORS 추가
+    'django.middleware.common.CommonMiddleware',  # CORS 추가
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
