06b63ae96f6cda46aa56bf42a3662fd3f49cc781
gwang1234 <qoi11@naver.com>
2025-01-26 16:54:04 +0900

    pdf 읽기

--- translation/hanja/views.py
+++ translation/hanja/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from PyPDF2 import PdfReader, PdfWriter
 
 # Create your views here.
 def index(request):
     return render(request, 'hanja/main.html')
 
 def upload_pdf(request):
-    print('ssssssssssss')
+    if request.method == 'POST' and request.FILES.get('pdf'):
+        pdf_file = request.FILES.get('pdf')
+        reader = PdfReader(pdf_file)
+        
+        page = reader.pages[0]
+        text = page.extract_text()
+        lines = text.split('\n')
+        
+        for line in lines:
+            print(line)
+            
+        print(lines[1])
+        
+    
     return HttpResponse("fdgdf")
