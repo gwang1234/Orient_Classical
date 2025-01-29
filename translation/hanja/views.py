37b32c2c07cf06044174c819556b314d3697e8fe
gwang1234 <qoi11@naver.com>
2025-01-29 23:54:11 +0900

    pdf 테스트

--- translation/hanja/views.py
+++ translation/hanja/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from PyPDF2 import PdfReader, PdfWriter
+import re
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 # Create your views here.
 def index(request):
     return render(request, 'hanja/main.html')
 
 def upload_pdf(request):
-    hanja = {'學' : 1,'而' : 2,'編' : 3}
+
     if request.method == 'POST' and request.FILES.get('pdf'):
         pdf_file = request.FILES.get('pdf')
         reader = PdfReader(pdf_file)
         
-        page = reader.pages[0]
-        text = page.extract_text()
-        lines = text.split('\n')
+        pages = reader.pages
+        for page in pages:
+            text = page.extract_text()
+            lines = text.split('\n')
+            for line in lines:
+                # 한글, 숫자, 영어, 특수문자 제거
+                only_hanja = re.sub(r'[A-z가-힣0-9\s!@#$%^&*()_+=\-:;,.<>?/|\\{}[\] ]', '', line)
+                if (only_hanja == ""):
+                    continue
+                else:
+                    print(only_hanja)
             
-        for line in lines:
-            print(line)
-            
-        print(lines[1])
         
     
     return HttpResponse("fdgdf")
