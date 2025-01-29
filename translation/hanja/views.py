d60f51214f4266d5910dcb33a39f2d15f0faf129
gwang1234 <qoi11@naver.com>
2025-01-30 01:26:24 +0900

    pdf 생성

--- translation/hanja/views.py
+++ translation/hanja/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2 import PdfReader
 import re
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.platypus import BaseDocTemplate, PageTemplate, Frame
+from reportlab.lib.units import cm
+from reportlab.platypus import Spacer
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -15,6 +24,25 @@ def index(request):
     return render(request, 'hanja/main.html')
 
 def upload_pdf(request):
+    
+    # 폰트 지정 
+    pdfmetrics.registerFont(TTFont("맑은고딕", "malgun.ttf"))
+
+    # 프레임 지정 
+    text_frame = Frame(
+                x1=2.54 * cm ,  # From left
+                y1=2.54 * cm ,  # From bottom
+                height=24.16 * cm,
+                width=15.92 * cm,
+                leftPadding=0 * cm,
+                bottomPadding=0 * cm,
+                rightPadding=0 * cm,
+                topPadding=0 * cm,
+                showBoundary=0,
+                id='text_frame'
+            )
+    
+    L=[]
 
     if request.method == 'POST' and request.FILES.get('pdf'):
         pdf_file = request.FILES.get('pdf')
@@ -27,11 +55,20 @@ def upload_pdf(request):
             for line in lines:
                 # 한글, 숫자, 영어, 특수문자 제거
                 only_hanja = re.sub(r'[A-z가-힣0-9\s!@#$%^&*()_+=\-:;,.<>?/|\\{}[\] ]', '', line)
-                if (only_hanja == ""):
+                if line.strip() == "":
                     continue
                 else:
-                    print(only_hanja)
+                    if (only_hanja == ""):
+                        L.append(Paragraph(line, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
+                    else:
+                        L.append(Paragraph(line, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
+                        L.append(Paragraph(only_hanja, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
             
-        
+        doc = BaseDocTemplate(str('hanja_test.pdf'), pagesize=A4)
+        frontpage = PageTemplate(id='FrontPage',
+                                frames=[text_frame]    # 4. 프레임 지정 부분에서 text_frame 생성했음
+                    )
+        doc.addPageTemplates(frontpage)
+        doc.build(L)
     
     return HttpResponse("fdgdf")
