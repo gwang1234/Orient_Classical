2eeb3c3f24584fab058dd095bfaa16d5450e9590
gwang1234 <qoi11@naver.com>
2025-01-31 18:45:00 +0900

    번역 완료

--- translation/hanja/views.py
+++ translation/hanja/views.py
@@ -11,19 +11,19 @@ from reportlab.platypus import Paragraph
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import BaseDocTemplate, PageTemplate, Frame
 from reportlab.lib.units import cm
+from reportlab.platypus import Spacer
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
+from urllib.parse import quote
 
 # Create your views here.
 def index(request):
-    hanja_entry = Hanja.objects.get(hanja='一').mean
-    print(hanja_entry)
-    
     return render(request, 'hanja/main.html')
 
 def upload_pdf(request):
@@ -60,21 +60,47 @@ def upload_pdf(request):
                 only_hanja = re.sub(r'[A-z가-힣0-9\s!@#$%^&*()_+=\-:;,.<>?/|\\{}[\] ]', '', line)
                 if line.strip() == "":
                     continue
+                    # L.append(Spacer(1, 20))
                 else:
                     if (only_hanja == ""):
                         L.append(Paragraph(line, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
                     else:
-                        # L.append(Paragraph(line, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
-                        # analysis = "( "
-                        # for hanja in only_hanja:
-                        #     try:
-                        #         hanja_entry = Hanja.objects.get(hanja=hanja)
-                        #         mean = hanja_entry.mean
-                        #     except Hanja.DoesNotExist:
-                        #         mean = None  
-                        L.append(Paragraph(only_hanja, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
+                        L.append(Paragraph(line, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
+                        analysis = "[ "
+                        
+                        for hanja in only_hanja:
+                            try:
+                                hanja_entry = Hanja.objects.get(hanja=hanja).mean
+                                analysis += "( " + hanja + ": " + hanja_entry + ") "
+                                
+                            except Hanja.DoesNotExist:
+                                if hanja.strip() == "" :
+                                    hanja_encoded = quote(hanja)
+                                    driver = webdriver.Chrome()
+                                    driver.get(f'https://hanja.dict.naver.com/#/search?query={hanja_encoded}')
+                                    WebDriverWait(driver, 60).until(
+                                        ec.presence_of_element_located((By.CSS_SELECTOR, '#searchLetterPage_content'))
+                                    )
+                                    
+                                    html = driver.page_source
+                                    soup = BeautifulSoup(html, 'html.parser')
+                                    
+                                    mean = soup.select_one(".mean").text.strip()
+                                    stroke = soup.find('div', string='총 획수').find_next_sibling().text.split("획")[0]
+                                    
+                                    analysis += "( " + hanja + ": " + mean + ") " 
+                                    Hanja.objects.create(hanja=hanja, mean=mean, stroke=stroke)
+                                
+                            except Hanja.MultipleObjectsReturned:
+                                hanja_entry = Hanja.objects.filter(hanja=hanja).first().mean
+                                analysis += "( " + hanja + ": " + hanja_entry + ") "
+                        
+                        analysis += "]"      
+                        L.append(Paragraph(analysis, ParagraphStyle(name='fd',fontName='맑은고딕',fontSize=11,leading=20)))
+                        L.append(Spacer(1, 20))
+                        
             
-        doc = BaseDocTemplate(str('hanja_test.pdf'), pagesize=A4)
+        doc = BaseDocTemplate(str('만법전.pdf'), pagesize=A4)
         frontpage = PageTemplate(id='FrontPage',
                                 frames=[text_frame]    # 4. 프레임 지정 부분에서 text_frame 생성했음
                     )
